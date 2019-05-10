0c38e126ab3036885a9580aeee7314167aac9814
cwern40 <chris.wernli@hotmail.com>
2019-05-09 18:26:03 -0600

    Created the TabLink component and added the event listener

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,24 +3,24 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
-    
+    this.element = element;
     // Get the custom data attribute on the Link
-    // this.data;
-    
+    // this.data; data-tab="1"
+    this.data.dataset.tab; 
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    
+    this.itemElement = document.querySelector(`.tabs.link[data-tab="${this.element.dataset.tab}"]`);
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    
+    this.tabItem = new TabItem(this.itemElement);
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () => this.select());
   };
 
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-
+    
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
 
@@ -59,4 +59,7 @@ class TabItem {
 
 */
 
-// links = document.querySelectorAll();
+const links = document.querySelectorAll('.tabs-link');
+links.forEach(link => {
+  new TabLink(link);
+})
